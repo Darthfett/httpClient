4556eff3bca2b5b9146303dbf9c22a7b777426bd
Casey Kuball <darthfett@gmail.com>
2013-02-15 21:34:33 -0700

    Initiate connection with localhost working

--- httpget.c
+++ httpget.c
@@ -2,7 +2,35 @@
 #include <stdlib.h>
 #include <string.h>
 
+#include <sys/types.h>
 #include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <unistd.h>
+
+#define HOST_ADDRESS "127.0.0.1"
+#define PORT 8000
+
+int sockfd;
+struct sockaddr_in address;
+
+int main(int argc, char *argv[]) {
+    sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    if (sockfd < 0) {
+        printf("Unable to open socket\n"); 
+        exit(1);
+        return 1;
+    }
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = inet_addr(HOST_ADDRESS);
+    address.sin_port = htons(PORT);
+
+    if (connect(sockfd, (struct sockaddr*) &address, sizeof(address)) < 0) {
+        printf("Unable to connect to host\n");
+        exit(1);
+        return 1;
+    }
+}
 
 void print_usage() {
     printf("Usage: ./httpget http://address/file.extension\n");
@@ -10,6 +38,9 @@ void print_usage() {
 
 char* split_path(char *path, char **file_out) {
     char *pos = strrchr(path, '/');
+    if (pos == NULL) {
+        
+    }
     int len_addr = pos - path;
     int len_file = &path[strlen(path)-1] - ++pos;
     
@@ -25,11 +56,16 @@ char* split_path(char *path, char **file_out) {
     return addr;
 }
 
-int main(int argc, char *argv[]) {
+int dontrunme(int argc, char *argv[]) {
     int arg_count = argc - 1;
     if (arg_count < 1 || arg_count > 1) {
         print_usage();
         return 1;
+    }
+
+    char *addr_start = argv[1];
+    if (strncmp(addr_start, "http://", strlen("http://")) == 0) {
+        addr_start += strlen("http://");
     }
 
     char *file_name;
